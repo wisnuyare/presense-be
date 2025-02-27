1a6a1b2cc9a23a4e252eb500f4bb8d34e9d68398
wisnuyare <wisnuyare@gmail.com>
2025-02-27 19:22:29 +0700

    remove log

--- src/server.ts
+++ src/server.ts
@@ -10,31 +10,10 @@ import authRoutes from "./routes/authRoutes";
 import attendanceRoutes from "./routes/attendanceRoutes";
 import employeeRoutes from "./routes/employeeRoutes";
 import uploadRoutes from "./routes/uploadRoutes";
-import db from "./config/db";
 
 dotenv.config();
 
 const app = express();
-
-async function testDatabaseConnection(retryCount = 0) {
-  try {
-      const connection = await db.getConnection();
-      const [rows] = await connection.execute('SELECT DATABASE()');
-      connection.release();
-      console.log('Database connection test:', rows);
-  } catch (error: any) {
-      if (error.code === 'EAI_AGAIN' && retryCount < 3) { // Retry up to 3 times
-          console.log(`Retry database connection (${retryCount + 1})...`);
-          await new Promise(resolve => setTimeout(resolve, Math.pow(2, retryCount) * 1000)); // Exponential backoff
-          await testDatabaseConnection(retryCount + 1);
-      } else {
-          console.error('Database connection test failed:', error);
-      }
-  }
-}
-
-// Call this function when your application starts or on a specific route
-
 
 app.use(
   cors({
@@ -71,5 +50,4 @@ app.use("/api", uploadRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
-  testDatabaseConnection();
 });
