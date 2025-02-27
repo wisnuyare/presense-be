162e1fc655fe31e73c6e4441793957da31ecad7d
wisnuyare <wisnuyare@gmail.com>
2025-02-27 20:45:27 +0700

    fix db host

--- src/server.ts
+++ src/server.ts
@@ -10,35 +10,17 @@ import authRoutes from "./routes/authRoutes";
 import attendanceRoutes from "./routes/attendanceRoutes";
 import employeeRoutes from "./routes/employeeRoutes";
 import uploadRoutes from "./routes/uploadRoutes";
-import db from "./config/db";
 
 dotenv.config();
 
 const app = express();
 
-async function testDatabaseConnection() {
-  console.log('Attempting database connection...');
-  try {
-      const connection = await db.getConnection();
-      const [rows] = await connection.execute('SELECT DATABASE()');
-      connection.release();
-      console.log('Database connection successful:', rows);
-  } catch (error) {
-      console.error('Database connection failed:', error);
-  }
-}
-
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
   })
 );
-
-// Handle preflight requests
-app.options('*', cors());
 app.use(
   helmet({
     crossOriginResourcePolicy: { policy: "cross-origin" },
@@ -63,9 +45,4 @@ app.use("/api", uploadRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
-  console.log('DB_HOST:', process.env.DB_HOST);
-  console.log('DB_USER:', process.env.DB_USER);
-  console.log('DB_NAME:', process.env.DB_NAME);
-  console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-  testDatabaseConnection()
 });
